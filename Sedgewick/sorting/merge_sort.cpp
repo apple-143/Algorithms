a99c47d3c5ea1bd8fefc79dcf7632a6958f70a36
root <root@ip-172-31-21-245.ap-northeast-2.compute.internal>
2019-07-23 11:01:56 +0900

    merge complete

--- Sedgewick/sorting/merge_sort.cpp
+++ Sedgewick/sorting/merge_sort.cpp
@@ -68,7 +68,7 @@ namespace merge
 		DefaultCompare comp;
 		int n = end - begin;
 		vector<Value> aux(n);
-		sort(begin, end, aux.begin(), aux.end(), 0, n, comp);
+		sort(begin, end, aux.begin(), aux.end(), 0, n-1, comp);
 	}
 
 	template <typename RandomAccessIterator, typename Comp>
@@ -78,7 +78,7 @@ namespace merge
 		using Value = decltype(t);
 		int n = end - begin;
 		vector<Value> aux(n);
-		sort(begin, end, aux.begin(), aux.end(), 0, n, comp);
+		sort(begin, end, aux.begin(), aux.end(), 0, n-1, comp);
 	}
 }
 
@@ -86,21 +86,54 @@ class CustomCompare
 {
 private:
 public:
-	bool operator()(const int& a, const int& b)
+	template <typename Comparable>
+	bool operator()(const Comparable& a, const Comparable& b)
 	{
 		return b < a;
 	}
 };
 
-void print_vector(vector<int> a)
+template <typename T>
+void print_vector(T a)
 {
 	for (int i = 0; i < a.size(); i++)
 		cout << a[i] << " ";
 	cout << endl;
 }
 
+class CustomClass
+{
+private:
+public:
+	int a;
+	int b;
+
+	CustomClass() : a(0), b(0)
+	{ }
+	CustomClass(const int& x, const int& y) : a(x), b(y)
+	{ }
+
+	bool operator<(const CustomClass& w) const
+	{
+		if (a < w.a)
+			return true;
+		if (a == w.a)
+			if (b < w.b)
+				return true;
+
+		return false;
+	}
+};
+
+std::ostream& operator<<(ostream& os, const CustomClass& x)
+{
+	os << "(" << x.a << ", " << x.b << ")";
+	return os;
+}
+
 int main()
 {
+	// int test
 	vector<int> a;
 	a.push_back(1);
 	a.push_back(2);
@@ -114,15 +147,59 @@ int main()
 	a.push_back(4);
 	a.push_back(9);
 	a.push_back(3);
+	CustomCompare my_compare;
 
 	print_vector(a);
 
 	merge::sort(a.begin(), a.end());
 	print_vector(a);
 
-	CustomCompare my_compare;
 	merge::sort(a.begin(), a.end(), my_compare);
 	print_vector(a);
+
+	// string test
+	vector<string> b;
+	b.push_back("a");
+	b.push_back("ab");
+	b.push_back("c");
+	b.push_back("d");
+	b.push_back("abc");
+	b.push_back("f");
+	b.push_back("king");
+	b.push_back("h");
+	b.push_back("i");
+	b.push_back("j");
+	b.push_back("k");
+	b.push_back("l");
+
+	print_vector(b);
+
+	merge::sort(b.begin(), b.end());
+	print_vector(b);
+
+	merge::sort(b.begin(), b.end(), my_compare);
+	print_vector(b);
+
+
+	// custom object test
+	vector<CustomClass> c;
+
+	c.push_back(CustomClass(1, 2));
+	c.push_back(CustomClass(3, 7));
+	c.push_back(CustomClass(2, 2));
+	c.push_back(CustomClass(1, 2));
+	c.push_back(CustomClass(2, 6));
+	c.push_back(CustomClass(1, 3));
+	c.push_back(CustomClass(3, 9));
+	c.push_back(CustomClass(1, 8));
+
+	print_vector(c);
+
+	merge::sort(c.begin(), c.end());
+	print_vector(c);
+
+	merge::sort(c.begin(), c.end(), my_compare);
+	print_vector(c);
 
 	return 0;
 }
