63ffc43e302e67ba864fcaf27028107d77b657a5
taewoo1991 <taewoo.kim.f.r@gmail.com>
2019-10-11 18:48:09 +0900

    added AddEdge function

--- Sedgewick/graph/graph.h
+++ Sedgewick/graph/graph.h
@@ -16,32 +16,47 @@ done
 template <typename T>
 class Bag {
 private:
+	std::vector<T> bag_;
 public:
-}
+	Bag () {
+	}
+
+	T operator[] (size_t idx) {
+		return bag_[idx];
+	}
+
+	void add (T x) {
+		bag_.push_back(x);
+	}
+};
 
 class Graph {
 private:
-	const int V;
-	int E;
+	const int kV_;
+	int E_;
 	Bag<int> * adj;
 
 public:
-	Graph(int V) : V(V) {
+	Graph (int V) : kV_(V), E_(0) {
+		adj = new Bag<int>[V];
 	}
 
-	Graph() {
+	int V () { return kV_; }
+	int E () { return E_; }
+
+	void AddEdge (int v, int w) {
+		adj[v].add(w);
+		adj[w].add(v);
+		E_++;
+	}
+	int TestAddEdge (int x, int i) {
+		return adj[x][i];
 	}
 
-	int V() { return V; }
-	int E() { return E; }
-
-	void AddEdge(int v, int w) {
+	void Adj (int v) {
 	}
 
-	void Adj(int v) {
-	}
-
-	std::string ToString() {
+	std::string ToString () {
 	}
 };
 
