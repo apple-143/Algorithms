2b547135ea08def693a50839b62d1bb6f90342c7
taewoo1991 <taewoo.kim.f.r@gmail.com>
2019-10-18 03:35:36 +0900

    added search.h

---
+++ Sedgewick/graph/search.h
@@ -0,0 +1,21 @@
+#ifndef SEARCH_TW_H_
+#define SEARCH_TW_H_
+
+#include "./graph.h"
+
+/*TODO
+*/
+class Search {
+private:
+public:
+	Search(Graph G, int s) {
+	}
+
+	bool Marked(const int& v) {
+	}
+
+	int Count() {
+	}
+};
+
+#endif	// SEARCH_TW_H_
