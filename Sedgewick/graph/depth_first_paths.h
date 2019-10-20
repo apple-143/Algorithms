9891803d0b4aa869ac8c1f992b258bbe8f516742
taewoo1991 <taewoo.kim.f.r@gmail.com>
2019-10-21 01:19:15 +0900

    completed depth first paths

--- Sedgewick/graph/depth_first_paths.h
+++ Sedgewick/graph/depth_first_paths.h
@@ -1,6 +1,7 @@
 #ifndef DEPTH_FIRST_PATH_TW_H_
 #define DEPTH_FIRST_PATH_TW_H_
 
+#include <list>
 #include "./graph.h"
 
 /*TODO
@@ -12,13 +13,14 @@ private:
 	const int s_;
 
 	void DFS(const Graph& G, int v) {
-/*
-		marked[v] = true;
-		++count;
-		for (auto it=G.Adj(v).begin(); it!=G.Adj(v).end(); ++it)
-			if (!marked[*it])
+		marked_[v] = true;
+
+		for (auto it=G.Adj(v).begin(); it!=G.Adj(v).end(); ++it) {
+			if (!marked_[*it]) {
+				edge_to_[*it] = v;
 				DFS(G, *it);
-*/
+			}
+		}
 	}
 
 public:
@@ -27,7 +29,7 @@ public:
 		edge_to_ = new int[G.V()];
 
 		for (int i=0; i<G.V(); ++i)
-			marked[i] = false;
+			marked_[i] = false;
 		DFS(G, s);
 	}
 	~DepthFirstPaths() {
@@ -35,16 +37,18 @@ public:
 		delete[] edge_to_;
 	}
 
-	bool Marked(const int& v) {
-/*
-		return marked[v];
-*/
+	bool HasPathTo(const int& v) {
+		return marked_[v];
 	}
 
-	int Count() {
-/*
-		return count;
-*/
+	std::list<int> PathTo(const int& v) {
+		if (!HasPathTo(v))
+			throw -1;
+		std::list<int> path;
+		for (int x=v; x!=s_; x=edge_to_[x])
+			path.push_front(x);
+		path.push_front(s_);
+		return path;
 	}
 };
 
