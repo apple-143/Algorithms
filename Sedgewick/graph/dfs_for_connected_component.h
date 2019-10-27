80cb226d283c500c79f650a92af2dc8f30775948
taewoo1991 <taewoo.kim.f.r@gmail.com>
2019-10-28 01:38:59 +0900

    complete DFS for CC

--- Sedgewick/graph/dfs_for_connected_component.h
+++ Sedgewick/graph/dfs_for_connected_component.h
@@ -7,46 +7,47 @@
 */
 class CC {
 private:
-/*
 	bool * marked_;
 	int count_;
-*/
+	int * id_;
 
 	void DFS(const Graph& G, int v) {
-/*
 		marked_[v] = true;
-		++count_;
+		id_[v] = count_;
 		for (auto it=G.Adj(v).begin(); it!=G.Adj(v).end(); ++it)
 			if (!marked_[*it])
 				DFS(G, *it);
-*/
 	}
 
 public:
-	CC(const Graph& G) {
-/*
+	CC(const Graph& G) : count_(0) {
 		marked_ = new bool[G.V()];
 		for (int i=0; i<G.V(); ++i)
 			marked_[i] = false;
-		DFS(G, s);
-*/
+		id_ = new int[G.V()];
+
+		for (int i=0; i<G.V(); ++i) {
+			if (!marked_[i]) {
+				DFS(G, i);
+				++count_;
+			}
+		}	
 	}
 	~CC() {
-/*
 		delete[] marked_;
-*/
+		delete[] id_;
 	}
 
-	bool Marked(const int& v) {
-/*
-		return marked_[v];
-*/
+	bool Connected(const int& v, const int& w) {
+		return id_[v] == id_[w];
+	}
+
+	int ID(const int& v) {
+		return id_[v];
 	}
 
 	int Count() {
-/*
 		return count_;
-*/
 	}
 };
 
