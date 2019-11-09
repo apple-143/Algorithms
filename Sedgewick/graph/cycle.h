369ed96ec4cd75a2c33db5c5227752c91dfcb3cb
taewoo1991 <taewoo.kim.f.r@gmail.com>
2019-11-09 21:12:12 +0900

    complete cycle

--- Sedgewick/graph/cycle.h
+++ Sedgewick/graph/cycle.h
@@ -7,61 +7,35 @@
 */
 class Cycle {
 private:
-/*
 	bool * marked_;
-	int count_;
-	int * id_;
-*/
+	bool has_cycle_;
 
-	void DFS(const Graph& G, int v) {
-/*
+	void DFS(const Graph& G, int v, int w) {
 		marked_[v] = true;
-		id_[v] = count_;
-		for (auto it=G.Adj(v).begin(); it!=G.Adj(v).end(); ++it)
+		for (auto it=G.Adj(v).begin(); it!=G.Adj(v).end(); ++it) {
 			if (!marked_[*it])
-				DFS(G, *it);
-*/
+				DFS(G, *it, v);
+			else if (*it != w)
+				has_cycle_ = true;
+		}
 	}
 
 public:
-	Cycle(const Graph& G) : count_(0) {
-/*
+	Cycle(const Graph& G) : has_cycle_(false) {
 		marked_ = new bool[G.V()];
 		for (int i=0; i<G.V(); ++i)
 			marked_[i] = false;
-		id_ = new int[G.V()];
 
-		for (int i=0; i<G.V(); ++i) {
-			if (!marked_[i]) {
-				DFS(G, i);
-				++count_;
-			}
-		}	
-*/
+		for (int i=0; i<G.V(); ++i)
+			if (!marked_[i])
+				DFS(G, i, i);
 	}
 	~Cycle() {
-/*
 		delete[] marked_;
-		delete[] id_;
-*/
 	}
 
-	bool Connected(const int& v, const int& w) {
-/*
-	return id_[v] == id_[w];
-*/
-	}
-
-	int ID(const int& v) {
-/*
-	return id_[v];
-*/
-	}
-
-	int Count() {
-/*
-	return count_;
-*/
+	bool HasCycle() {
+		return has_cycle_;
 	}
 };
 
