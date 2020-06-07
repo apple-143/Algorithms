2f3381df976686dfd9ee5bcfd833148a99dcffee
least-action <taewoo.kim.f.r@gmail.com>
2020-06-07 15:23:08 +0000

    added basic sorting: selction, insertion

---
+++ sedgewick/src/main/sorting/BasicSorting.java
@@ -0,0 +1,54 @@
+package sorting;
+
+import java.lang.UnsupportedOperationException;
+
+import edu.princeton.cs.algs4.StdOut;
+
+abstract class BasicSorting {
+    abstract public void sort(Comparable[] a);
+
+    protected static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    protected static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    protected static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; ++i)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    protected static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; ++i)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+}
+
+class Selection extends BasicSorting {
+    public void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; ++i) {
+            int minIdx = i;
+            for (int j = i+1; j < N; ++j) {
+                if (less(a[j], a[minIdx])) minIdx = j;
+            }
+            exch(a, i, minIdx);
+        }
+    }
+}
+
+class Insertion extends BasicSorting {
+    public void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 1; i < N; ++i) {
+            for (int j = i; j > 0 && less(a[j], a[j-1]); --j)
+                exch(a, j, j-1);
+        }
+    }
+}
