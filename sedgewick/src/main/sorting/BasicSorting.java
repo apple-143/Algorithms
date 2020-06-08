bcf7585557e6ad13836f44e3d8c53c4b2cdd8cd9
least-action <taewoo.kim.f.r@gmail.com>
2020-06-08 18:38:32 +0000

    made sort methos to be static

--- sedgewick/src/main/sorting/BasicSorting.java
+++ sedgewick/src/main/sorting/BasicSorting.java
@@ -4,8 +4,10 @@ import java.lang.UnsupportedOperationException;
 
 import edu.princeton.cs.algs4.StdOut;
 
-abstract class BasicSorting {
-    abstract public void sort(Comparable[] a);
+class BasicSorting {
+    public static void sort(Comparable[] a) {
+        return;
+    }
 
     protected static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
@@ -31,7 +33,7 @@ abstract class BasicSorting {
 }
 
 class Selection extends BasicSorting {
-    public void sort(Comparable[] a) {
+    public static void sort(Comparable[] a) {
         int N = a.length;
         for (int i = 0; i < N; ++i) {
             int minIdx = i;
@@ -44,7 +46,7 @@ class Selection extends BasicSorting {
 }
 
 class Insertion extends BasicSorting {
-    public void sort(Comparable[] a) {
+    public static void sort(Comparable[] a) {
         int N = a.length;
         for (int i = 1; i < N; ++i) {
             for (int j = i; j > 0 && less(a[j], a[j-1]); --j)
