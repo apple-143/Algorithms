baf2c22cec81012cb0eeb62ae2560d978e3967b3
least-action <taewoo.kim.f.r@gmail.com>
2020-06-09 17:24:13 +0000

    added Merge sort

--- sedgewick/src/main/sorting/SortCompare.java
+++ sedgewick/src/main/sorting/SortCompare.java
@@ -6,7 +6,7 @@ import fundamentals.Stopwatch;
 import sorting.Insertion;
 import sorting.Selection;
 //import sorting.Shell;
-//import sorting.Merge;
+import sorting.Merge;
 //import sorting.Quick;
 //import sorting.Heap;
 
@@ -17,7 +17,7 @@ public class SortCompare {
         if (alg.equals("Insertion"))    Insertion.sort(a);
         if (alg.equals("Selection"))    Selection.sort(a);
 //        if (alg.equals("Shell"))        Shell.sort(a);
-//        if (alg.equals("Merge"))        Merge.sort(a);
+        if (alg.equals("Merge"))        Merge.sort(a);
 //        if (alg.equals("Quick"))        Quick.sort(a);
 //        if (alg.equals("Heap"))         Heap.sort(a);
         return timer.elapsedTime();
