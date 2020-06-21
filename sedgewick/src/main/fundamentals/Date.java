cf63d06ee4927b28fa6c868bd3f25680064b44e8
least-action <taewoo.kim.f.r@gmail.com>
2020-06-21 15:35:10 +0000

    added Date and Transaction classes

---
+++ sedgewick/src/main/fundamentals/Date.java
@@ -0,0 +1,53 @@
+package fundamentals;
+
+public class Date implements Comparable<Date> {
+    private final int month;
+    private final int day;
+    private final int year;
+
+    public Date(int m, int d, int y) {
+        month = m;
+        day = d;
+        year = y;
+    }
+
+    public Date(String date) {
+        String[] fields = date.split("/");
+        month = Integer.parseInt(fields[0]);
+        day   = Integer.parseInt(fields[1]);
+        year  = Integer.parseInt(fields[2]);
+    }
+
+    public int month()
+    { return month; }
+
+    public int day()
+    { return day; }
+
+    public int year()
+    { return year; }
+
+    public String toString()
+    { return month() + "/" + day() + "/" + year(); }
+
+    public boolean equals(Object x) {
+        if (this == x) return true;
+        if (x == null) return false;
+        if (this.getClass() != x.getClass()) return false;
+        Date that = (Date) x;
+        if (this.day != that.day)       return false;
+        if (this.month != that.month)   return false;
+        if (this.year != that.year)     return false;
+        return true;
+    }
+
+    public int compareTo(Date that) {
+        if (this.year() < that.year()) return 1;
+        if (this.year() > that.year()) return -1;
+        if (this.month() < that.month()) return 1;
+        if (this.month() > that.month()) return -1;
+        if (this.day() < that.day()) return 1;
+        if (this.day() > that.day()) return -1;
+        return 0;
+    }
+}
---
+++ sedgewick/src/main/fundamentals/Transaction.java
@@ -0,0 +1,53 @@
+package fundamentals;
+
+import fundamentals.Date;
+
+public class Transaction implements Comparable<Transaction> {
+    private final String who;
+    private final Date when;
+    private final double amount;
+
+    public Transaction(String who, Date when, double amount) {
+        this.who = who;
+        this.when = when;
+        this.amount = amount;
+    }
+
+    public Transaction(String transaction) {
+        String[] fields = transaction.split(" ");
+        who = fields[0];
+        when = new Date(fields[1]);
+        amount = Double.parseDouble(fields[2]);
+    }
+
+    public String who()
+    { return who; }
+
+    public Date when()
+    { return when; }
+
+    public double amount
+    { return amount; }
+
+    public String toString() {
+        return who + " " + when + " " + amount;
+    }
+
+    boolean equals(Object x) {
+        if (this == x) return true;
+        if (x == null) return false;
+        if (this.getClass() != x.getClass()) return false;
+        Transaction that = (Transaction) x;
+        if (this.who != that.who)       return false;
+        if (this.when != that.when)     return false;
+        if (this.amount != that.amount) return false;
+        return true;
+ 
+    }
+
+    int compareTo(Transaction that) {
+        if (this.amount > that.amount) return 1;
+        if (this.amount < that.amount) return -1;
+        return 0;
+    }
+}
